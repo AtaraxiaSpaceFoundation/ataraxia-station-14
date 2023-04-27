07bc0bb754585f4985c8132df00e4e04b3a626d0
rhailrake <49613070+rhailrake@users.noreply.github.com>
2023-04-28 04:44:00 +0600

    Asay for Meaty Ores

--- Content.Server/Chat/Systems/ChatSystem.cs
+++ Content.Server/Chat/Systems/ChatSystem.cs
@@ -10,6 +10,7 @@ using Content.Server.Station.Components;
 using Content.Server.Station.Systems;
 using Content.Server.UtkaIntegration;
 using Content.Shared.ActionBlocker;
+using Content.Shared.Administration;
 using Content.Shared.CCVar;
 using Content.Shared.Chat;
 using Content.Shared.Database;
@@ -605,7 +606,7 @@ public sealed partial class ChatSystem : SharedChatSystem
     {
         var name = FormattedMessage.EscapeText(Identity.Name(source, EntityManager));
 
-        if (_adminManager.IsAdmin(player))
+        if (_adminManager.HasAdminFlag(player, AdminFlags.Admin))
         {
             if (!AdminLoocEnabled)
                 return;
