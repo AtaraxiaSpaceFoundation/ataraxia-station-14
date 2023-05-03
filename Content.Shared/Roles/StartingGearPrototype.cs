4bf54ed149822a93698276fa3f0362b2cc668138
RavMorgan <48182970+RavMorgan@users.noreply.github.com>
2023-05-03 12:13:54 +0300

    [feature] ТРУСЫ (#12)
    * Трусы и бра
    
    * Тэги и тэмплейты инвентаря
    
    * Спрайты
    
    * Тэги и темплейт
    
    * Флаги
    
    * Перевод и прототипы
    
    * Иконки
    
    * Добавил слои
    
    * Исправил циферки во флагах
    
    * НОСКИ
    
    * Трусы всем
    
    * Блять
    
    * СПРАЙТЫ БРА
    
    * Насрано
    
    * х2
    
    * Примерно миллион трусов
    
    * fff
    
    * Носки(дохуя)
    
    * Сука
    
    * ааааааааааааааааааа
    
    * Ебаные трусы блять
    
    * Фикс иконок
    
    ---------
    
    Co-authored-by: Mona Hmiza <>

--- Content.Shared/Roles/StartingGearPrototype.cs
+++ Content.Shared/Roles/StartingGearPrototype.cs
@@ -1,3 +1,4 @@
+using Content.Shared.Humanoid;
 using Content.Shared.Preferences;
 using Robust.Shared.Prototypes;
 
@@ -24,6 +25,14 @@ namespace Content.Shared.Roles
         [DataField]
         public List<EntProtoId> Inhand = new(0);
 
+        // White underwear
+        [DataField("underweart")]
+        private string _underweart = string.Empty;
+
+        [DataField("underwearb")]
+        private string _underwearb = string.Empty;
+        // White underwear end
+
         [ViewVariables]
         [IdDataField]
         public string ID { get; private set; } = string.Empty;
@@ -38,6 +47,12 @@ namespace Content.Shared.Roles
                     return Satchel;
                 if (slot == "back" && profile.Backpack == BackpackPreference.Duffelbag && !string.IsNullOrEmpty(Duffelbag))
                     return Duffelbag;
+                // White underwear
+                if (slot == "underweart" && profile.Sex == Sex.Female && !string.IsNullOrEmpty(_underweart))
+                    return _underweart;
+                if (slot == "underwearb" && profile.Sex == Sex.Female && !string.IsNullOrEmpty(_underwearb))
+                    return _underwearb;
+                // White underwear end
             }
 
             return Equipment.TryGetValue(slot, out var equipment) ? equipment : string.Empty;
