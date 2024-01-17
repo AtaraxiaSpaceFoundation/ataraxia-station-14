12bdddcbd96d79bc89973074b85ac08191591750
Aviu00 <aviu00@protonmail.com>
2024-01-17 10:10:07 +0300

    Fix?

--- Content.Server/White/Other/CustomFluffSystems/merkka/CustomCatExamineComponent.cs
+++ Content.Server/White/Other/CustomFluffSystems/merkka/CustomCatExamineComponent.cs
@@ -1,7 +1,7 @@
 namespace Content.Server.White.Other.CustomFluffSystems.merkka;
 
 [RegisterComponent]
-public sealed class CustomCatExamineComponent : Component
+public sealed partial class CustomCatExamineComponent : Component
 {
     public string? CatOwner;
 }
