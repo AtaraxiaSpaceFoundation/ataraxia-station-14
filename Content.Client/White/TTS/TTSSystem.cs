frosty-dev/ss14-amour

Raised amount of tries before timeout, convert wav to ogg

Content.Client/White/TTS/TTSSystem.cs
@@ -117,7 +117,7 @@ public sealed class TTSSystem : EntitySystem
     private bool TryCreateAudioSource(byte[] data, [NotNullWhen(true)] out IClydeAudioSource? source)
     {
         var dataStream = new MemoryStream(data) { Position = 0 };
-        var audioStream = _clyde.LoadAudioWav(dataStream);
+        var audioStream = _clyde.LoadAudioOggVorbis(dataStream);
         source = _clyde.CreateAudioSource(audioStream);
         source?.SetVolume(_volume);
         return source != null;

AtaraxiaSpaceFoundation/ataraxia-station-14

Content.Client/White/TTS/TTSSystem.cs
@@ -115,7 +115,7 @@ public sealed class TTSSystem : EntitySystem
     private bool TryCreateAudioSource(byte[] data, [NotNullWhen(true)] out IAudioSource? source)
     {
         var dataStream = new MemoryStream(data) { Position = 0 };
-        var audioStream = _audioSystem.LoadAudioWav(dataStream);
+        var audioStream = _audioSystem.LoadAudioOggVorbis(dataStream);
         source = _audioSystem.CreateAudioSource(audioStream);
         if (source == null)
         {