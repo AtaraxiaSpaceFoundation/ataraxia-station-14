frosty-dev/ss14-amour

[Feat] War crime console (#144)
* ADD: Icons, New component. Надо будет рефакторнуть худы в одну систему

* Some govno ebanoe

* ui

* Some govno

* UI and UI lol

* Dermo again

* ы

* Добавлена система консоли. Надо добавить манипуляцию с рекордами и сохранение крим. записей на сервер. Я пометил в туду

* Added functional for Criminal Records UI

* Дропаю это говно

* Рабочая версия крим консоли

* Fuull functional

* Added radio

* Arrest info feature

* improve ui

* another names

* New texturem Sprite viewer

* fix small names

* Added login menu

* Final fix.

* ох

* Убрал логгеры

* fix Comments and Access to proto

* moved dummy code, removed qustyions

* added disposer() when window was close

* Small fixes

* Removed comments. Added DNA check for CriminalityHud

* Small lol

---------

Co-authored-by: DocNITE <docnite0530@gmail.com>

Content.Client/White/CriminalRecords/UI/CriminalRecordsBoundUserInterface.cs
@@ -1,0 +1,75 @@
+using Content.Shared.Containers.ItemSlots;
+using Content.Shared.StationRecords;
+using Content.Shared.White.CriminalRecords;
+using Robust.Client.GameObjects;
+using Robust.Shared.Input;
+using Robust.Shared.Utility;
+
+namespace Content.Client.White.CriminalRecords.UI;
+
+public sealed class CriminalRecordsBoundUserInterface : BoundUserInterface
+{
+    private CriminalRecordsWindow? _window = default!;
+
+    public CriminalRecordsBoundUserInterface(ClientUserInterfaceComponent owner, Enum uiKey) : base(owner, uiKey)
+    {}
+
+    protected override void Open()
+    {
+        base.Open();
+
+        _window = new();
+        _window.OnKeySelected += OnKeySelected;
+        _window.OnStatusSelected += OnStatusSelected;
+        _window.OnTextBindDown += OnTextEntered;
+        _window.LogOutButton.Controller.OnPressed += _ =>
+        {
+            SendMessage(new ItemSlotButtonPressedEvent(CriminalRecordsConsoleComponent.IdSlotId));
+        };
+        _window.LogInButton.Controller.OnPressed += _ =>
+        {
+            SendMessage(new ItemSlotButtonPressedEvent(CriminalRecordsConsoleComponent.IdSlotId));
+        };
+        _window.OnClose += Close;
+
+        _window.OpenCentered();
+    }
+
+    private void OnKeySelected(StationRecordKey key)
+    {
+        SendMessage(new SelectCriminalRecord(key));
+    }
+
+    private void OnStatusSelected(StationRecordKey key, CriminalRecordInfo status)
+    {
+        SendMessage(new SelectCriminalStatus(key, status));
+    }
+
+    private void OnTextEntered(StationRecordKey key, string text)
+    {
+        if (!string.IsNullOrEmpty(text))
+        {
+            SendMessage(new SelectCriminalReason(key, text));
+        }
+    }
+
+    protected override void UpdateState(BoundUserInterfaceState state)
+    {
+        base.UpdateState(state);
+
+        if (state is not CriminalRecordsConsoleBuiState cast)
+        {
+            return;
+        }
+
+        _window?.UpdateState(cast);
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        base.Dispose(disposing);
+
+        _window?.Close();
+        _window?.Dispose();
+    }
+}

AtaraxiaSpaceFoundation/ataraxia-station-14

Content.Client/White/CriminalRecords/UI/CriminalRecordsBoundUserInterface.cs
@@ -1,0 +1,75 @@
+using Content.Shared.Containers.ItemSlots;
+using Content.Shared.StationRecords;
+using Content.Shared.White.CriminalRecords;
+using Robust.Client.GameObjects;
+using Robust.Shared.Input;
+using Robust.Shared.Utility;
+
+namespace Content.Client.White.CriminalRecords.UI;
+
+public sealed class CriminalRecordsBoundUserInterface : BoundUserInterface
+{
+    private CriminalRecordsWindow? _window = default!;
+
+    public CriminalRecordsBoundUserInterface(EntityUid owner, Enum uiKey) : base(owner, uiKey)
+    {}
+
+    protected override void Open()
+    {
+        base.Open();
+
+        _window = new();
+        _window.OnKeySelected += OnKeySelected;
+        _window.OnStatusSelected += OnStatusSelected;
+        _window.OnTextBindDown += OnTextEntered;
+        _window.LogOutButton.Controller.OnPressed += _ =>
+        {
+            SendMessage(new ItemSlotButtonPressedEvent(CriminalRecordsConsoleComponent.IdSlotId));
+        };
+        _window.LogInButton.Controller.OnPressed += _ =>
+        {
+            SendMessage(new ItemSlotButtonPressedEvent(CriminalRecordsConsoleComponent.IdSlotId));
+        };
+        _window.OnClose += Close;
+
+        _window.OpenCentered();
+    }
+
+    private void OnKeySelected(StationRecordKey key)
+    {
+        SendMessage(new SelectCriminalRecord(key));
+    }
+
+    private void OnStatusSelected(StationRecordKey key, CriminalRecordInfo status)
+    {
+        SendMessage(new SelectCriminalStatus(key, status));
+    }
+
+    private void OnTextEntered(StationRecordKey key, string text)
+    {
+        if (!string.IsNullOrEmpty(text))
+        {
+            SendMessage(new SelectCriminalReason(key, text));
+        }
+    }
+
+    protected override void UpdateState(BoundUserInterfaceState state)
+    {
+        base.UpdateState(state);
+
+        if (state is not CriminalRecordsConsoleBuiState cast)
+        {
+            return;
+        }
+
+        _window?.UpdateState(cast);
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        base.Dispose(disposing);
+
+        _window?.Close();
+        _window?.Dispose();
+    }
+}