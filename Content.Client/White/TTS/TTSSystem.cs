frosty-dev/ss14-amour

[Fix] TTS announcement volume fix (#344)

Content.Client/White/TTS/TTSSystem.cs
@@ -102,7 +102,10 @@ public sealed class TTSSystem : EntitySystem
 
     private void OnPlayTTS(PlayTTSEvent ev)
     {
-        if (!TryCreateAudioSource(ev.Data, out var source))
+        var volume = 0f;
+        if (ev.BoostVolume)
+            volume = 5f;
+        if (!TryCreateAudioSource(ev.Data, out var source, volume))
             return;
 
         var stream = new AudioStream(ev.Uid, source);
@@ -114,12 +117,12 @@ public sealed class TTSSystem : EntitySystem
         RaiseNetworkEvent(new RequestTTSEvent(text));
     }
 
-    private bool TryCreateAudioSource(byte[] data, [NotNullWhen(true)] out IClydeAudioSource? source)
+    private bool TryCreateAudioSource(byte[] data, [NotNullWhen(true)] out IClydeAudioSource? source, float volume = 0f)
     {
         var dataStream = new MemoryStream(data) { Position = 0 };
         var audioStream = _clyde.LoadAudioOggVorbis(dataStream);
         source = _clyde.CreateAudioSource(audioStream);
-        source?.SetVolume(_volume);
+        source?.SetVolume(volume == 0f ? _volume : volume);
         return source != null;
     }
 

Content.Shared/White/TTS/PlayTTSEvent.cs
@@ -8,10 +8,12 @@ public sealed class PlayTTSEvent : EntityEventArgs
 {
     public EntityUid Uid { get; }
     public byte[] Data { get; }
+    public bool BoostVolume { get; }
 
-    public PlayTTSEvent(EntityUid uid, byte[] data)
+    public PlayTTSEvent(EntityUid uid, byte[] data, bool boostVolume)
     {
         Uid = uid;
         Data = data;
+        BoostVolume = boostVolume;
     }
 }

AtaraxiaSpaceFoundation/ataraxia-station-14

Content.Client/White/TTS/TTSSystem.cs
@@ -100,7 +100,10 @@ public sealed class TTSSystem : EntitySystem
 
     private void OnPlayTTS(PlayTTSEvent ev)
     {
-        if (!TryCreateAudioSource(ev.Data, out var source))
+        var volume = 0f;
+        if (ev.BoostVolume)
+            volume = 5f;
+        if (!TryCreateAudioSource(ev.Data, out var source, volume))
             return;
 
         var stream = new AudioStream(GetEntity(ev.Uid), source);
@@ -112,7 +115,7 @@ public sealed class TTSSystem : EntitySystem
         RaiseNetworkEvent(new RequestTTSEvent(text));
     }
 
-    private bool TryCreateAudioSource(byte[] data, [NotNullWhen(true)] out IAudioSource? source)
+    private bool TryCreateAudioSource(byte[] data, [NotNullWhen(true)] out IAudioSource? source, float volume = 0f)
     {
         var dataStream = new MemoryStream(data) { Position = 0 };
         var audioStream = _audioSystem.LoadAudioOggVorbis(dataStream);
@@ -122,7 +125,7 @@ public sealed class TTSSystem : EntitySystem
             return false;
         }
 
-        source.Volume = _volume;
+        source.Volume = volume == 0f ? _volume : volume;
         return true;
     }
 

Content.Shared/White/TTS/PlayTTSEvent.cs
@@ -8,10 +8,12 @@ public sealed class PlayTTSEvent : EntityEventArgs
 {
     public NetEntity Uid { get; }
     public byte[] Data { get; }
+    public bool BoostVolume { get; }
 
-    public PlayTTSEvent(NetEntity uid, byte[] data)
+    public PlayTTSEvent(NetEntity uid, byte[] data, bool boostVolume)
     {
         Uid = uid;
         Data = data;
+        BoostVolume = boostVolume;
     }
 }