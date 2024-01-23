frosty-dev/ss14-amour

Fixes & Tweaks (#356)
* Revert "captains sword reflect chance (#18133)"

This reverts commit e393eedd09b0cfc3d2498a813f039f57963f77eb.

* Mirror shield passive reflect

* Traitor stuff fixes and tweaks

* Connected dispenser click sound

* No medipen heavy attack

* Slice sound for blades

* Crossbow & material stacking fixes

* Hypospray fixes

* Chenge crossbow damage type

* Fix penetrating on land

* Antispam tweak

Content.Server/Chat/Managers/ChatManager.cs
@@ -185,9 +185,9 @@ namespace Content.Server.Chat.Managers
             _utkaSocketWrapper.SendMessageToAll(asayEventMessage);
         }
 
-        public bool TrySendNewMessage(IPlayerSession session, string newMessage)
+        public bool TrySendNewMessage(IPlayerSession session, string newMessage, bool checkLength = false)
         {
-            if (!_antispam || newMessage.Length < _antispamMinLength)
+            if (!_antispam || checkLength && newMessage.Length < _antispamMinLength)
             {
                 _lastMessages.Remove(session.Data.UserId);
                 return true;

Content.Server/Chat/Managers/IChatManager.cs
@@ -25,7 +25,7 @@ namespace Content.Server.Chat.Managers
 
         // WD-EDIT
         void SendHookAdminChat(string sender, string message);
-        bool TrySendNewMessage(IPlayerSession session, string newMessage);
+        bool TrySendNewMessage(IPlayerSession session, string newMessage, bool checkLength = false);
         // WD-EDIT
 
         void SendAdminAnnouncement(string message);

AtaraxiaSpaceFoundation/ataraxia-station-14

Content.Server/Chat/Managers/ChatManager.cs
@@ -216,9 +216,9 @@ namespace Content.Server.Chat.Managers
             _utkaSocketWrapper.SendMessageToAll(asayEventMessage);
         }
 
-        public bool TrySendNewMessage(ICommonSession session, string newMessage)
+        public bool TrySendNewMessage(ICommonSession session, string newMessage, bool checkLength = false)
         {
-            if (!_antispam || newMessage.Length < _antispamMinLength)
+            if (!_antispam || checkLength && newMessage.Length < _antispamMinLength)
             {
                 _lastMessages.Remove(session.Data.UserId);
                 return true;

Content.Server/Chat/Managers/IChatManager.cs
@@ -24,7 +24,7 @@ namespace Content.Server.Chat.Managers
 
         // WD-EDIT
         void SendHookAdminChat(string sender, string message);
-        bool TrySendNewMessage(ICommonSession session, string newMessage);
+        bool TrySendNewMessage(ICommonSession session, string newMessage, bool checkLength = false);
         // WD-EDIT
 
         void SendAdminAnnouncement(string message);