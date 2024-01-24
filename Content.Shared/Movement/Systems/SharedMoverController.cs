frosty-dev/ss14-amour

[Sponsor] M4ury fluff (#460)

Content.Shared/Movement/Systems/SharedMoverController.cs
@@ -421,6 +421,15 @@ namespace Content.Shared.Movement.Systems
                 return true;
             }
 
+            //WD start
+            if (_inventory.TryGetSlotEntity(uid, "neck", out var neck)
+                && TryComp<FootstepModifierComponent>(neck, out var modifierComponent))
+            {
+                sound = modifierComponent.Sound;
+                return true;
+            }
+            //WD end
+
             return TryGetFootstepSound(uid, xform, shoes != null, out sound);
         }
 

AtaraxiaSpaceFoundation/ataraxia-station-14

Content.Shared/Movement/Systems/SharedMoverController.cs
@@ -451,6 +451,15 @@ namespace Content.Shared.Movement.Systems
                 return true;
             }
 
+            //WD start
+            if (_inventory.TryGetSlotEntity(uid, "neck", out var neck)
+                && TryComp<FootstepModifierComponent>(neck, out var modifierComponent))
+            {
+                sound = modifierComponent.FootstepSoundCollection;
+                return true;
+            }
+            //WD end
+
             return TryGetFootstepSound(uid, xform, shoes != null, out sound, tileDef: tileDef);
         }
 