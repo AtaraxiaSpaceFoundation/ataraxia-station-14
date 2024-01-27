8e4e099469d6e58136659fe7eb396d823ab4f04c
Aviu00 <aviu00@protonmail.com>
2024-01-27 12:38:50 +0300

    Revert "make status icons unshaded (#18974)"
    This reverts commit 23ad53c1e09f5efde96f2756668f6e08d592ff26.

--- Content.Client/StatusIcon/StatusIconOverlay.cs
+++ Content.Client/StatusIcon/StatusIconOverlay.cs
@@ -4,19 +4,16 @@ using Robust.Client.GameObjects;
 using Robust.Client.Graphics;
 using Robust.Shared.Enums;
 using System.Numerics;
-using Robust.Shared.Prototypes;
 
 namespace Content.Client.StatusIcon;
 
 public sealed class StatusIconOverlay : Overlay
 {
     [Dependency] private readonly IEntityManager _entity = default!;
-    [Dependency] private readonly IPrototypeManager _prototype = default!;
 
     private readonly SpriteSystem _sprite;
     private readonly TransformSystem _transform;
     private readonly StatusIconSystem _statusIcon;
-    private readonly ShaderInstance _shader;
 
     public override OverlaySpace Space => OverlaySpace.WorldSpaceBelowFOV;
 
@@ -27,7 +24,6 @@ public sealed class StatusIconOverlay : Overlay
         _sprite = _entity.System<SpriteSystem>();
         _transform = _entity.System<TransformSystem>();
         _statusIcon = _entity.System<StatusIconSystem>();
-        _shader = _prototype.Index<ShaderPrototype>("unshaded").Instance();
     }
 
     protected override void Draw(in OverlayDrawArgs args)
@@ -39,8 +35,6 @@ public sealed class StatusIconOverlay : Overlay
         var xformQuery = _entity.GetEntityQuery<TransformComponent>();
         var scaleMatrix = Matrix3.CreateScale(new Vector2(1, 1));
         var rotationMatrix = Matrix3.CreateRotation(-eyeRot);
-
-        handle.UseShader(_shader);
 
         var query = _entity.AllEntityQueryEnumerator<StatusIconComponent, SpriteComponent, TransformComponent, MetaDataComponent>();
         while (query.MoveNext(out var uid, out var comp, out var sprite, out var xform, out var meta))
@@ -105,7 +99,5 @@ public sealed class StatusIconOverlay : Overlay
                 handle.DrawTexture(texture, position);
             }
         }
-
-        handle.UseShader(null);
     }
 }
