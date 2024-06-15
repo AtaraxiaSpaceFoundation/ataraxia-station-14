e90ffe397348b98c1791b5643dda54ea4433a7fd
Aviu00 <93730715+Aviu00@users.noreply.github.com>
2024-06-15 00:47:49 +0000

    Spell descriptions (#352)
    * - add: Spell descriptions and more.
    
    * - add: Update wizard shuttle.
    
    * - add: Add wizard to secret.
    
    * - add: Call shuttle on wizard death.

--- Content.Server/_White/Wizard/WizardRuleComponent.cs
+++ Content.Server/_White/Wizard/WizardRuleComponent.cs
@@ -50,7 +50,7 @@ public sealed partial class WizardRuleComponent : Component
     [DataField]
     public ProtoId<NpcFactionPrototype> Faction = "Wizard";
 
-    public RoundEndBehavior RoundEndBehavior = RoundEndBehavior.Nothing;
+    public RoundEndBehavior RoundEndBehavior = RoundEndBehavior.ShuttleCall;
 
     [DataField]
     public string RoundEndTextSender = "comms-console-announcement-title-centcom";
