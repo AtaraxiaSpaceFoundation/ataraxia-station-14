41a1a6a1ea22d7bde9235d1173341317eb6b5fae
trest100 <macro200481@gmail.com>
2024-06-30 12:31:21 +0300

    Non Non peaceful RE

--- Content.Server/_White/AspectsSystem/Aspects/BattledAspect.cs
+++ Content.Server/_White/AspectsSystem/Aspects/BattledAspect.cs
@@ -6,7 +6,6 @@ using Content.Server._White.AspectsSystem.Base;
 using Content.Shared.Hands.Components;
 using Content.Shared.Hands.EntitySystems;
 using Content.Shared.Humanoid;
-using Content.Shared._White.NonPeacefulRoundEnd;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Random;
 
@@ -18,33 +17,9 @@ public sealed class BattledAspect : AspectSystem<BattledAspectComponent>
     [Dependency] private readonly SharedHandsSystem _handsSystem = default!;
     [Dependency] private readonly IRobustRandom _robustRandom = default!;
 
-    private NonPeacefulRoundItemsPrototype _nonPeacefulRoundItemsPrototype = default!;
 
-    public override void Initialize()
-    {
-        base.Initialize();
 
-        SubscribeLocalEvent<PlayerSpawnCompleteEvent>(HandleLateJoin);
-    }
 
-    protected override void Started(EntityUid uid, BattledAspectComponent component, GameRuleComponent gameRule, GameRuleStartedEvent args)
-    {
-        base.Started(uid, component, gameRule, args);
-
-        var prototypes =  _prototypeManager.EnumeratePrototypes<NonPeacefulRoundItemsPrototype>().ToList();
-
-        if (prototypes.Count == 0)
-            ForceEndSelf(uid, gameRule);
-
-        _nonPeacefulRoundItemsPrototype = _robustRandom.Pick(prototypes);
-
-        var query = EntityQueryEnumerator<HumanoidAppearanceComponent>();
-        while (query.MoveNext(out var ent, out _))
-        {
-            GiveItem(ent);
-        }
-
-    }
 
     private void HandleLateJoin(PlayerSpawnCompleteEvent ev)
     {
@@ -59,29 +34,9 @@ public sealed class BattledAspect : AspectSystem<BattledAspectComponent>
 
             var mob = ev.Mob;
 
-            GiveItem(mob);
+
         }
     }
 
-    #region Helpers
 
-    private void GiveItem(EntityUid player)
-    {
-        var item = _robustRandom.Pick(_nonPeacefulRoundItemsPrototype.Items);
-
-        var transform = CompOrNull<TransformComponent>(player);
-
-        if(transform == null)
-            return;
-
-        if(!HasComp<HandsComponent>(player))
-            return;
-
-        var weaponEntity = EntityManager.SpawnEntity(item, transform.Coordinates);
-
-        _handsSystem.TryDrop(player);
-        _handsSystem.PickupOrDrop(player, weaponEntity);
-    }
-
-    #endregion
 }
