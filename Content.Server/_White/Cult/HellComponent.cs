eced84e98e825d10bac270816bc726859614947d
rhailrake <splatt.pr@gmail.com>
2024-07-05 23:14:06 +0500

    master

---
+++ Content.Server/_White/Cult/HellComponent.cs
@@ -0,0 +1,7 @@
+﻿using Content.Shared._White.Cult.Pentagram;
+using Robust.Shared.GameStates;
+
+namespace Content.Server._White.Cult;
+
+[NetworkedComponent, RegisterComponent]
+public sealed partial class HellComponent : SharedHellComponent;
